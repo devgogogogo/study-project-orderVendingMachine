a4876ef38d2514950b880ef22a86ef27e7c91a78
KwiHyeon <imleo322@gmail.com>
2025-07-29 03:37:11 +0900

    first commit

---
+++ src/main/java/com/example/orderkiosk/OrderkioskApplication.java
@@ -0,0 +1,13 @@
+package com.example.orderkiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderkioskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderkioskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/orderkiosk/OrderkioskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orderkiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderkioskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
